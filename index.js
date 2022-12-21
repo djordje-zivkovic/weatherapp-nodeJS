e518514add263e0c6abbd28d01bb84f17f713d56
djordje-zivkovic <djordjezivkovic@protonmail.com>
2022-12-21 15:02:29 +0100

    basic setup of route and controller

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import express from "express";
+const express = require("express");
+const weatherRouter = require("./routes/weatherRoute");
+
 const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.use("/api/weather", weatherRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    message: "Can't find this route on server",
+  });
 });
 
 app.listen(port, () => {
