179da891a922807294097740c09805e46334692a
djordje-zivkovic <djordjezivkovic@protonmail.com>
2023-01-05 11:59:54 +0100

    error handling implementation

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { all } = require("./routes/weatherRoute");
-const weatherRouter = require("./routes/weatherRoute");
+const weatherRouter = require("./routes/weather.route");
 const { getData } = require("./utils/api.js");
 const cities = require("./utils/cities");
+const AppError = require("./utils/appError");
 
 const app = express();
 const port = 4000;
@@ -10,9 +10,7 @@ const port = 4000;
 app.use("/api/weather", weatherRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    message: "Can't find this route on server",
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.listen(port, () => {
