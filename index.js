d1a955f8094415b56a3f9eee227e8899261ce427
djordje-zivkovic <djordjezivkovic@protonmail.com>
2023-01-09 12:46:50 +0100

    database setup

--- index.js
+++ index.js
@@ -4,8 +4,28 @@ const { getData } = require("./utils/api.js");
 const cities = require("./utils/cities");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/error.controller");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("MYSQL Connected...");
+  }
+});
+
 const port = 4000;
 
 app.use("/api/weather", weatherRouter);
