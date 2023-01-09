1b7b7ccae91512a4d0dc0dd62f7db65b3998f824
djordje-zivkovic <djordjezivkovic@protonmail.com>
2023-01-09 09:56:21 +0100

    add globalError handler

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const weatherRouter = require("./routes/weather.route");
 const { getData } = require("./utils/api.js");
 const cities = require("./utils/cities");
 const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/error.controller");
 
 const app = express();
 const port = 4000;
@@ -12,6 +13,8 @@ app.use("/api/weather", weatherRouter);
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
