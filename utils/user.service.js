2bbef44bb96b94782d2c836789f7e46752c2dad0
djordje-zivkovic <djordjezivkovic@protonmail.com>
2023-01-09 15:11:51 +0100

    add routes and controllers

---
+++ utils/user.service.js
@@ -0,0 +1,80 @@
+const pool = require("../config/database");
+
+module.exports = {
+  create: (data, callBack) => {
+    pool.query(
+      `insert into users(name, email, password) 
+                  values(?,?,?)`,
+      [data.name, data.email, data.password],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  getUserByUserEmail: (email, callBack) => {
+    pool.query(
+      `SELECT * FROM users WHERE email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUserByUserId: (id, callBack) => {
+    pool.query(
+      `SELECT id,name,email from users where id = ?`,
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUsers: (callBack) => {
+    pool.query(
+      `SELECT id,name,email FROM users`,
+      [],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  updateUser: (data, callBack) => {
+    pool.query(
+      `UPDATE users SET name=?, email=?, password=? WHERE id = ?`,
+      [data.name, data.email, data.password, data.id],
+      (error, results, fields) => {
+        console.log(results);
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  deleteUser: (data, callBack) => {
+    console.log(data.id);
+    pool.query(
+      `DELETE FROM users WHERE id = ?`,
+      [data.id],
+      (error, results, fields) => {
+        console.log(results);
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+};
